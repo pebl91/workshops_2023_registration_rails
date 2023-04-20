94a5d2e4a16987a391386b84b86480a3abe0e6e6
Paweł Smusz <pawel_smusz@yahoo.com>
2023-04-20 18:19:41 +0200

    fix for first problem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,6 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
     if @task.save
       redirect_to tasks_url, notice: 'Task successfully created.'
     else
@@ -31,6 +30,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:description)
+    params.require(:task).permit(:title, :description)
   end
 end
